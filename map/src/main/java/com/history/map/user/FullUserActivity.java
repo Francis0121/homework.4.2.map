9c0f571169052b95886b9ec42daefeaa325b8f22
Francis0121 <tjdrms0121@gmail.com>
2015-03-16 11:00:21 +0900

    Full Screen Start page
    And
    
    User Login Activity

---
+++ map/src/main/java/com/history/map/user/FullUserActivity.java
@@ -0,0 +1,17 @@
+package com.history.map.user;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.history.map.R;
+
+public class FullUserActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_user);
+    }
+
+}
