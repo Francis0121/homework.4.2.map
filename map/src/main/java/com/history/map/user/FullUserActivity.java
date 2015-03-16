920c4ce5c63cc33f8f8af73cc7d8ef50ecff33c9
Francis0121 <tjdrms0121@gmail.com>
2015-03-16 18:02:49 +0900

    Adjust Google Map API

--- map/src/main/java/com/history/map/user/FullUserActivity.java
+++ map/src/main/java/com/history/map/user/FullUserActivity.java
@@ -6,12 +6,10 @@ import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import com.history.map.R;
-import com.history.map.position.FullPositionActivity;
+import com.history.map.position.MapsActivity;
 import com.history.map.util.SystemUiHider;
 
 public class FullUserActivity extends ActionBarActivity {
@@ -103,7 +101,7 @@ public class FullUserActivity extends ActionBarActivity {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FullPositionActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
                 startActivity(intent);
             }
         });
