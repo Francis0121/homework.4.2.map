8374a6874b09df193eed7749c5b73d661a47a830
Francis0121 <tjdrms0121@gmail.com>
2015-03-17 21:37:45 +0900

    위치 정보에 따른 활성화 시키기

--- map/src/main/java/com/history/map/user/FullUserActivity.java
+++ map/src/main/java/com/history/map/user/FullUserActivity.java
@@ -1,139 +1,55 @@
 package com.history.map.user;
 
-import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
-import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
 import com.history.map.R;
-import com.history.map.position.MapsActivity;
-import com.history.map.util.SystemUiHider;
 
-public class FullUserActivity extends ActionBarActivity {
+public class FullUserActivity extends Activity implements View.OnClickListener {
 
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_user);
 
-        final View controlsView = findViewById(R.id.fullscreen_user_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_user_content);
-
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-            // Cached values.
-            int mControlsHeight;
-            int mShortAnimTime;
-
-            @Override
-            @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-            public void onVisibilityChange(boolean visible) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                    if (mControlsHeight == 0) {
-                        mControlsHeight = controlsView.getHeight();
-                    }
-                    if (mShortAnimTime == 0) {
-                        mShortAnimTime = getResources().getInteger(
-                                android.R.integer.config_shortAnimTime);
-                    }
-                } else {
-                    // If the ViewPropertyAnimator APIs aren't
-                    // available, simply show or hide the in-layout UI
-                    // controls.
-                    controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                }
-
-                if (visible && AUTO_HIDE) {
-                    // Schedule a hide().
-                    delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                }
-            }
-        });
-
-        // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
-        });
-
-        Button button = (Button) findViewById(R.id.positionBtn);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
-                startActivity(intent);
-            }
-        });
+        findViewById(R.id.map01Btn).setOnClickListener(this);
+        findViewById(R.id.map02Btn).setOnClickListener(this);
+        findViewById(R.id.map03Btn).setOnClickListener(this);
+        findViewById(R.id.map04Btn).setOnClickListener(this);
         
     }
-
+    
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
+    public void onClick(View v) {
+        
+        Uri gmmIntentUri = null;
+    
+        switch(v.getId()) {
+            case R.id.map01Btn:
+                gmmIntentUri = Uri.parse("geo:37.56641923090,126.9778741551?q=천안 독립기념관");
+                break;
+            case R.id.map02Btn:
+                gmmIntentUri = Uri.parse("geo:37.56641923090,126.9778741551?q=전주한옥마을");
+                break;
+            case R.id.map03Btn:
+                gmmIntentUri = Uri.parse("geo:37.56641923090,126.9778741551?q=성산일출봉");
+                break;
+            case R.id.map04Btn:
+                gmmIntentUri = Uri.parse("geo:37.56641923090,126.9778741551?q=서울시청");
+                break;
         }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+        if(gmmIntentUri != null) {
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            startActivity(mapIntent);
+        }
     }
 
 }
