2b936d8263c4c3a88f196703882945b922e1233d
Francis0121 <tjdrms0121@gmail.com>
2015-03-17 22:41:25 +0900

    Map 정보 가져와서 Location 지정 까지 수행

--- map/src/main/java/com/history/map/position/MapsActivity.java
+++ map/src/main/java/com/history/map/position/MapsActivity.java
@@ -3,49 +3,80 @@ package com.history.map.position;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Build;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.history.map.R;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends Activity {
 
+    private String location;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_map);
-
+        Log.d("MapsActivity", "oncreate");
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
-        setUpMapIfNeeded();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
+        if(Geocoder.isPresent()){
+            try {
+                Intent receivedIntent = getIntent();
+                location = receivedIntent.getExtras().getString("uri");
 
+                Geocoder gc = new Geocoder(this);
+                List<Address> addresses= gc.getFromLocationName(location, 5); // get the found Address Objects
+
+                List<LatLng> ll = new ArrayList<LatLng>(addresses.size()); // A list to save the coordinates if they are available
+                for(Address a : addresses){
+                    if(a.hasLatitude() && a.hasLongitude()){
+                        ll.add(new LatLng(a.getLatitude(), a.getLongitude()));
+                    }
+                }
+                
+                if(ll.size() > 0){
+                    setUpMapIfNeeded(ll.get(0));
+                }
+            } catch (IOException e) {
+                // handle the exception
+            }
+        }
+
+    }
+    
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void setUpMapIfNeeded() {
+    private void setUpMapIfNeeded(LatLng latLng) {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
             FragmentManager fm = getFragmentManager();
             mMap = ((MapFragment) fm.findFragmentById(R.id.map)).getMap();
+            mMap.addMarker(new MarkerOptions().position(new LatLng(37.56641923090, 126.9778741551)).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14.0f));
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Toast toast = Toast.makeText(this, "Visited " + location, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
--- map/src/main/java/com/history/map/user/FullUserActivity.java
+++ map/src/main/java/com/history/map/user/FullUserActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import com.history.map.R;
+import com.history.map.position.MapsActivity;
 
 public class FullUserActivity extends Activity implements View.OnClickListener {
 
@@ -29,26 +30,27 @@ public class FullUserActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         
-        Uri gmmIntentUri = null;
+        String uri = null;
     
         switch(v.getId()) {
             case R.id.map01Btn:
-                gmmIntentUri = Uri.parse("geo:37.56641923090,126.9778741551?q=천안 독립기념관");
+                uri = "천안 독립기념관";
                 break;
             case R.id.map02Btn:
-                gmmIntentUri = Uri.parse("geo:37.56641923090,126.9778741551?q=전주한옥마을");
+                uri = "전주한옥마을";
                 break;
             case R.id.map03Btn:
-                gmmIntentUri = Uri.parse("geo:37.56641923090,126.9778741551?q=성산일출봉");
+                uri = "성산일출봉";
                 break;
             case R.id.map04Btn:
-                gmmIntentUri = Uri.parse("geo:37.56641923090,126.9778741551?q=서울시청");
+                uri = "서울시청";
                 break;
         }
-        if(gmmIntentUri != null) {
-            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-            mapIntent.setPackage("com.google.android.apps.maps");
-            startActivity(mapIntent);
+        if(uri != null) {
+            Intent intent = new Intent(this, MapsActivity.class);
+            intent.putExtra("uri", uri);
+            
+            startActivity(intent);
         }
     }
 
